1bc0ac7290838848ae5b59b8bb30a5ec5dd94825
Marcos Piñero <marcos@Marcoss-MacBook-Pro-2.local>
2023-09-04 11:54:36 -0300

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+declare module 'query-to-mongo' {
+  interface QueryToMongoOptions {
+    maxLimit?: string | number
+    ignore?: 'fields' | 'omit' | 'sort' | 'offset' | 'limit'
+    parser?: object
+  }
+
+  interface QueryToMongoResult<T> {
+    criteria: Omit<T, 'limit' | 'offset' | 'sort'>;
+    options: {
+      limit?: number,
+      skip?: number,
+      sort?: object
+    };
+    link: Function
+  }
+
+  export function queryToMongo<T>(query: T, options?: QueryToMongoOptions): QueryToMongoResult<T>;
+  
+}
